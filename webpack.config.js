5b0fd80d74e4745641e2475ad26ed043b1f9c08a
yuhongbo <953127874@qq.com>
2021-03-29 10:56:58 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, './src/js/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname + '/dist'),
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tpl$/,
+        loader: 'ejs-loader',
+        options: {
+          esModule: false
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: path.resolve(__dirname, 'node_modules')
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, './src/index.html'),
+      chunks: ['index'],
+      excludeChunks: ['node_modules']
+    })
+  ],
+  devServer: {
+    port: 8089,
+    host: 'localhost',
+    open: true
+  }
+}
